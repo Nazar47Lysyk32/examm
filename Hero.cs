8540015b20289286096546c5f332da90c1538917
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-02-07 14:52:19 +0200

    Add files via upload

---
+++ Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    
+    internal class Hero
+    {
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double AttackPower { get; set; }
+        public int ResistanceToPhysical { get; set; }
+        public int ResistanceToMagical { get; set; }
+
+
+        public Hero(string Name, double Health, double AttackPower, int ResistanceToPhysical, int ResistanceToMagical)
+        {
+            this.Name = Name;
+            this.Health = Health;
+            this.AttackPower = AttackPower;
+            this.ResistanceToPhysical = ResistanceToPhysical;
+            this.ResistanceToMagical = ResistanceToMagical;
+
+        }
+
+
+    }
+}
